079609e569bef16304b00e21fe7c1d39077b3e73
Marcin Puc <tranzystorek.io@protonmail.com>
2022-12-01 06:41:35 +0100

    Add solution for day 1

--- src/days/d01p2.rs
+++ src/days/d01p2.rs
@@ -4,7 +4,7 @@ use aoc_utils::BufferedInput;
 
 #[anyhoo::anyhoo]
 fn parse_input() -> Vec<Vec<usize>> {
-    let mut input = BufferedInput::parse_args("Day 1: Calorie Counting - Part 1")?;
+    let mut input = BufferedInput::parse_args("Day 1: Calorie Counting - Part 2")?;
 
     let mut s = String::new();
     input.read_to_string(&mut s).unwrap();
@@ -26,10 +26,16 @@ fn main() {
     let calories_carried = parse_input()?;
 
     aoc_utils::measure_and_print(|| {
-        calories_carried
+        let mut sums: Vec<_> = calories_carried
             .into_iter()
             .map(|elf| elf.into_iter().sum::<usize>())
-            .max()
-            .unwrap()
+            .collect();
+
+        sums.sort_unstable();
+
+        let index = sums.len() - 3;
+        let result: usize = sums[index..].iter().sum();
+
+        result
     });
 }
